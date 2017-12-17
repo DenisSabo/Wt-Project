bac6fe421ecac7d424819593e7027b7a1c0b9e1f
Ludwii <LudwigStud@outlook.de>
2017-12-17 18:11:45 +0100

    * seperated index route from server.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
+// node modules
 var express = require('express');
 var expressSession = require('express-session');
 var passport = require('passport');
 var config = require('config');
+
+// routes
+var indexRoute = require('./lib/index');
+var oauth2Route = require('./lib/oauth2');
+
+// init express
 var app = express();
 
-//set route for static files like css,js
+// set route for static files like css,js
 app.use('/public', express.static(__dirname + '/public'));
-
-app.get("/",function(req,res){
-    res.sendfile("./views/index.html");
-});
-
-app.listen(config.get("WebServer.port"), function(){
-    console.log(('Express server listening on port ' + config.get("WebServer.port")));
-});
 
 // [START session]
 // Configure the session and session storage.
@@ -30,4 +29,13 @@ app.use(expressSession(sessionConfig));
 // OAuth2
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(require('./lib/oauth2').router);
+app.use(oauth2Route.router);
+
+// first route
+app.use("/", indexRoute);
+
+// listen on port
+app.listen(config.get("WebServer.port"), function(){
+    console.log(('Express server listening on port ' + config.get("WebServer.port")));
+});
+
