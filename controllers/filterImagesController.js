fa123774ad9ccbc897a1e2f21bdfe617cf92096d
Denis Sabolotni <denissabolotni@gmail.com>
2018-01-14 20:57:50 +0100

    Filter for returning most popular images. Not tested. Gets all images safed in database, and sorts it by "popularity level". (Bubblesort is used for sorting)

--- controllers/filterImagesController.js
+++ controllers/filterImagesController.js
@@ -17,7 +17,7 @@ exports.image_user_list = function(req, res) {
 	Image.find({ 'user': googleUserID }, function(err, image){
 		if(err){
 			console.log(err);
-			res.status(400).send("Something went wrong: %s", err).end();
+			res.status(500).send("Something went wrong: %s", err).end();
 			return false;
 		}
 		else{
@@ -37,15 +37,38 @@ exports.image_category_list = function(req, res) {
 
 //Displays images sorted by popularity (Clicks + likes)
 exports.image_popular_list = function(req, res) {
-	res.send('NOT IMPLEMENTED: Most popular images');
+	Image.find({}, function(err, image){
+		if(err){
+			console.log(err);
+			res.status(500).send("Something went wrong: %s", err).end();
+			return false;
+		}
+		else{
+			/** BUBBLESORT */
+			//iterate through images
+			for(var x = 0; x < (image.length - 1); x++){ //Worst Case: Most popular image is the last item of array. n-1 iterations needed for bringing it at it's right position
+				//compare image[i] and next image[i+1]
+				for(var i = 0; i < (image.length - 1 - x); i++){ //x is the limit of the right side. After each iteration the limit moves from the right side, on element down to the left side
+					//sort images by popularity
+					var popularityLevel = image[i].clicks + (image[i].likes * 5); // likes are more important than clicks, so other weighting
+					var popularityLevelNext = image[i+1].clicks + (image[i + 1].likes * 5);
+					if(popularityLevelNext > popularityLevel){
+						//exchange: most popular will be in front
+						var cache = image[i]; 
+						image[i] = image[i + 1];
+						image[i + 1] = cache;
+					}
+				}
+			}
+
+			console.log("Sorted array of objects %s", image);
+			res.status(201).json(image).end();
+		}
+	})
 };
 
 //Displays images by given place
 exports.image_place_list = function(req, res) {
 	res.send('NOT IMPLEMENTED: Images taken in one place');
-};
-
-exports.image_all = function(req, res){
-	res.send('NOT IMPLEMENTED: All Images');
 };
 
