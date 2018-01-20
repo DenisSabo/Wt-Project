09d2f57e3124e06de715d7e354c6b5da000bcbe4
Denis Sabolotni <denissabolotni@gmail.com>
2018-01-20 22:46:53 +0100

    New filter: Recent images and all images. Prototypes for requesting only 9 images, not all at once.

--- controllers/filterImagesController.js
+++ controllers/filterImagesController.js
@@ -140,32 +140,34 @@ exports.image_popular_list_page = function(req, res){
 	pageNumber--; 
 
 	if(pageNumber !== null && pageNumber !== undefined){
-		Image.find({}, function(err, image){
-			if(err){
-			console.log(err);
-			res.status(500).send("Something went wrong: %s", err).end();
-		}
-		else{
-			/** BUBBLESORT */
-			//iterate through images
-			for(var x = 0; x < (image.length - 1); x++){ //Worst Case: Most popular image is the last item of array. n-1 iterations needed for bringing it at it's right position
-				//compare image[i] and next image[i+1]
-				for(var i = 0; i < (image.length - 1 - x); i++){ //x is the limit of the right side. After each iteration the limit moves from the right side, on element down to the left side
-					//sort images by popularity
-					var popularityLevel = image[i].clicks + (image[i].likes * 5); // likes are more important than clicks, so other weighting
-					var popularityLevelNext = image[i+1].clicks + (image[i + 1].likes * 5);
-					if(popularityLevelNext > popularityLevel){
-						//exchange: most popular will be in front
-						var cache = image[i]; 
-						image[i] = image[i + 1];
-						image[i + 1] = cache;
+			Image.find({}, function(err, image){
+				if(err){
+				console.log(err);
+				res.status(500).send("Something went wrong: %s", err).end();
+			}
+			else{
+				/** BUBBLESORT */
+				//iterate through images
+				for(var x = 0; x < (image.length - 1); x++){ //Worst Case: Most popular image is the last item of array. n-1 iterations needed for bringing it at it's right position
+					//compare image[i] and next image[i+1]
+					for(var i = 0; i < (image.length - 1 - x); i++){ //x is the limit of the right side. After each iteration the limit moves from the right side, on element down to the left side
+						//sort images by popularity
+						var popularityLevel = image[i].clicks + (image[i].likes * 5); // likes are more important than clicks, so other weighting
+						var popularityLevelNext = image[i+1].clicks + (image[i + 1].likes * 5);
+
+						if(popularityLevelNext > popularityLevel){
+							//exchange: most popular will be in front
+							var cache = image[i]; 
+							image[i] = image[i + 1];
+							image[i + 1] = cache;
+						}
 					}
 				}
-			}
 
-			console.log("Sorted array of objects %s", image);
-			res.status(200).json(image).end();
-		}
-	}).skip(pageNumber * 9).limit(9);
+				console.log("Sorted array of objects %s", image);
+				res.status(200).json(image).end();
+			}
+		}).skip(pageNumber * 9).limit(9);
+	}
 };
 
