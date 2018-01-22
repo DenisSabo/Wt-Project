9b3bef30affcd799f5f41842530abb93279a0587
Denis Sabolotni <denissabolotni@gmail.com>
2018-01-22 14:44:33 +0100

    Unnecessary service

--- controllers/services/errorHandlingService.js
+++
@@ -1,9 +0,0 @@
-var exports = module.exports = {};
-
-//closes connection and sends back a message
-exports.clientErrorHandling = function(statusCode, message, response){
-	response.status(statusCode).end(message);
-	console.log("Connection to client was closed with status " + statusCode);
-	console.log("Message to client send: '" + message + "'");
-	console.log("");
-}
