0af344203ea815db0640928c6c4cab79e9f20e1c
Denis Sabolotni <denissabolotni@gmail.com>
2018-01-23 16:25:55 +0100

    Better solution: No extra ajax. User will be created directly in index.js

--- controllers/manageUsersController.js
+++ controllers/manageUsersController.js
@@ -3,34 +3,7 @@ var User = require('../models/users.js');
 //If it is a new user: Safe his google data to database
 //Needed data in message body: req.user -> req.user.id and req.user.displayname
 exports.user_create_post = function(req, res) {
-	if(req.user){
-		var googleName = req.user.displayname;
-		var googleID = req.user.id;
-		var pathToPic = req.user.image;
-		var cursor = User.find({ googleUserID: googleID });
-
-		if(!cursor){
-			//cursor is empty = it's a new user
-			User.insert({ username: googleName, googleUserID: googleID, googlePicture: pathToPic }, function(err){
-				if(err){
-					console.log("Error while trying to safe new user: " + err);
-					res.status(404).end(err);
-				}
-				else{
-					console.log("New user added: " + googleName);
-					res.status(201).end("New user " + googleName + " was created.");
-				}
-			})
-		}
-		else{
-			//User already safed in database
-			res.send(409).end("User already exists: " + googleName);
-		}
-	}
-	else{
-		console.log("req.user was empty");
-		res.status(404).end("request.user data is missing. Try to gain information by using 'getAccountAPI'.");
-	}
+	//Is not needed anymore: index.js 
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+var User = require('../models/users.js');
 
 router.get('/', function (req, res){
-    if (req.user) res.sendfile(path.resolve('views/LoggedIn.html'));
-    else res.sendfile(path.resolve('views/index.html'));
+    if (req.user) {
+    	var cursor = User.find({ googleUserID: googleID });
+    	if(!cursor){
+			//cursor is empty = it's a new user
+			User.insert({ username: googleName, googleUserID: googleID, googlePicture: pathToPic }, function(err){
+				if(err){
+					console.log("Error while trying to safe new user: " + err);
+					//Validation failed
+					res.status(404)sendfile(path.resolve('views/index.html'));
+				}
+				else{
+					console.log("New user added: " + googleName);
+					res.sendfile(path.resolve('views/LoggedIn.html'));
+				}
+			})
+		}
+		else{
+			//User already safed in database
+			res.sendfile(path.resolve('views/LoggedIn.html'));
+		}
+
+    	
+    }
+    //no req.user data send
+    else res.status(404).sendfile(path.resolve('views/index.html'));
 });
 
 module.exports = router;
