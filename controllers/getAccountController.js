5f65c42f0c183a475e3199cf21c8f6d733e6b458
Denis Sabolotni <denissabolotni@gmail.com>
2018-01-24 20:16:35 +0100

    Also von dir hätte ich sowas nicht erwarten Ludwig ...

--- controllers/getAccountController.js
+++ controllers/getAccountController.js
@@ -3,7 +3,7 @@ const path = require('path');
 var User = require('../models/users.js');
 
 exports.default = function (req, res){
-    res.setHeader('Content-Rype', 'application/json');
+    res.setHeader('Content-Type', 'application/json');
     if (req.user) res.send(JSON.stringify(req.user));
     else res.send(JSON.stringify({"data": "undefined"}));
 }
