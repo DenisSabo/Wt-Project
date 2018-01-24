2ca98e606b371f79e03f65b0c02e70fbf8858d08
Ludwii <LudwigStud@outlook.de>
2018-01-24 20:04:52 +0100

    * cleaned server.js by splitting some routes into routes.js

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var index_controller = require('../controllers/indexController');
-
-router.get('/', index_controller.default);
-
-module.exports = router;
